97a16fd757b65f8d201a2f2f78032defb6c609b1
Cheems <arocano6813@uta.edu.ec>
2022-11-30 15:11:23 -0500

    exportaciones

--- appCMV/src/app/agencias/pages/page-lists/page-lists.component.ts
+++ appCMV/src/app/agencias/pages/page-lists/page-lists.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
+import { AgencyServiceService } from 'src/app/services/agency-service.service';
 import { DownloadComponent } from 'src/app/share/components/download/download.component';
 import { keypadButton } from 'src/app/share/interfaces/keypad.interface';
 import { MetaDataColumn } from 'src/app/share/interfaces/metacolumn.interface';
 import { environment } from 'src/environments/environment';
+
+
 
 @Component({
   selector: 'cmv-page-lists',
@@ -11,49 +14,12 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./page-lists.component.css']
 })
 export class PageListsComponent implements OnInit {
-records: any[] =[
-  {_id:1,name:'Ambato',address:'Calle A'},
-  {_id:2,name:'Riobamba',address:'Calle B'},
-  {_id:3,name:'Quito',address:'Calle C'},
-  {_id:4,name:'Cuena',address:'Calle D'},
-  {_id:5,name:'Guayaquil',address:'Calle E'},
-  {_id:6,name:'Ambato',address:'Calle A'},
-  {_id:7,name:'Riobamba',address:'Calle B'},
-  {_id:8,name:'Quito',address:'Calle C'},
-  {_id:9,name:'Cuena',address:'Calle D'},
-  {_id:10,name:'Guayaquil',address:'Calle E'},
-  {_id:11,name:'Ambato',address:'Calle A'},
-  {_id:12,name:'Riobamba',address:'Calle B'},
-  {_id:13,name:'Quito',address:'Calle C'},
-  {_id:14,name:'Cuena',address:'Calle D'},
-  {_id:15,name:'Guayaquil',address:'Calle E'},
-  {_id:16,name:'Ambato',address:'Calle A'},
-  {_id:17,name:'Riobamba',address:'Calle B'},
-  {_id:18,name:'Quito',address:'Calle C'},
-  {_id:19,name:'Cuena',address:'Calle D'},
-  {_id:20,name:'Guayaquil',address:'Calle E'},
-  {_id:21,name:'Ambato',address:'Calle A'},
-  {_id:22,name:'Riobamba',address:'Calle B'},
-  {_id:23,name:'Quito',address:'Calle C'},
-  {_id:24,name:'Cuena',address:'Calle D'},
-  {_id:25,name:'Guayaquil',address:'Calle E'},
-  {_id:26,name:'Ambato',address:'Calle A'},
-  {_id:27,name:'Riobamba',address:'Calle B'},
-  {_id:28,name:'Quito',address:'Calle C'},
-  {_id:29,name:'Cuena',address:'Calle D'},
-  {_id:30,name:'Guayaquil',address:'Calle E'},
-  {_id:31,name:'Ambato',address:'Calle A'},
-  {_id:32,name:'Riobamba',address:'Calle B'},
-  {_id:33,name:'Quito',address:'Calle C'},
-  {_id:34,name:'Cuena',address:'Calle D'},
-  {_id:35,name:'Guayaquil',address:'Calle E'},
- 
-]
-metaDataColumns:MetaDataColumn[]=[
-  {field:"_id",title:"ID"},
-  {field:"name",title:"AGENCIA"},
-  {field:"address",title:"DIRECCIÓN"},
-]
+
+  metaDataColumns: MetaDataColumn[] = [
+    { field: "_id", title: "Id" },
+    { field: "name", title: "Agencia" },
+    { field: "address", title: "Dirección" }
+  ];
 data:any[]=[]
 totalRecords=this.data.length
 keypadButton:keypadButton[]=[
@@ -66,7 +32,9 @@ keypadButton:keypadButton[]=[
   color:"primary",
   action:"new"},
 ]
-  constructor(private bottomSheet:MatBottomSheet) {
+records:any[]=[];
+  constructor(private bottomSheet:MatBottomSheet,private agencyService: AgencyServiceService) {
+    this.records=this.agencyService.getRecords();
     this.loadAgencies();
    }
 
@@ -103,4 +71,5 @@ keypadButton:keypadButton[]=[
   showBottomSheet(title: string, fileName: string, data: any) {
     this.bottomSheet.open(DownloadComponent);
   }
+
 }
--- appCMV/src/app/share/components/download/download.component.ts
+++ appCMV/src/app/share/components/download/download.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
+import * as XLSX from 'xlsx';
+import { AgencyServiceService } from 'src/app/services/agency-service.service';
 
 @Component({
   selector: 'cmv-download',
@@ -7,12 +14,91 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DownloadComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(private agencyService:AgencyServiceService) {
+
+  }
 
   ngOnInit(): void {
   }
-  export(){
-    
+
+  export() {
+
   }
 
+  openPdfTables() {
+
+    const documentDefinition = {
+      content: [
+        {
+          table: {
+            headerRows: 1,
+            widths: ['*', '*', '*'],
+            body: [
+              //[this.metaDataColumns[0].title, this.metaDataColumns[1].title, this.metaDataColumns[2].title],
+              //...this.records.map((item) => [item._id, item.name, item.address])
+              [this.agencyService.getMetaDataColumns()[0].title, this.agencyService.getMetaDataColumns()[1].title, this.agencyService.getMetaDataColumns()[2].title],
+              ...this.agencyService.getRecords().map((item) => [item._id, item.name, item.address])
+            ]
+          }
+        }
+      ]
+    };
+    pdfMake.createPdf(documentDefinition).open();
+  }
+
+  downloadPdfTables() {
+
+    const documentDefinition = {
+      content: [
+        {
+          table: {
+            headerRows: 1,
+            widths: ['*', '*', '*'],
+            body: [
+              //[this.metaDataColumns[0].title, this.metaDataColumns[1].title, this.metaDataColumns[2].title],
+              //...this.records.map((item) => [item._id, item.name, item.address])
+              [this.agencyService.getMetaDataColumns()[0].title, this.agencyService.getMetaDataColumns()[1].title, this.agencyService.getMetaDataColumns()[2].title],
+              ...this.agencyService.getRecords().map((item) => [item._id, item.name, item.address])
+            ]
+          }
+        }
+      ]
+    };
+    pdfMake.createPdf(documentDefinition).download();
+  }
+
+  printPdfTables() {
+    const documentDefinition = {
+      content: [
+        {
+          table: {
+            headerRows: 1,
+            widths: ['*', '*', '*'],
+            body: [
+              //[this.metaDataColumns[0].title, this.metaDataColumns[1].title, this.metaDataColumns[2].title],
+              //...this.records.map((item) => [item._id, item.name, item.address])
+              [this.agencyService.getMetaDataColumns()[0].title, this.agencyService.getMetaDataColumns()[1].title, this.agencyService.getMetaDataColumns()[2].title],
+              ...this.agencyService.getRecords().map((item) => [item._id, item.name, item.address])
+            ]
+          }
+        }
+      ]
+    };
+    pdfMake.createPdf(documentDefinition).print();
+  }
+
+  exportToExcel():void{
+    let name = 'Agencias.xlsx';
+    //let element = document.getElementById('season-tble');
+    let element=this.agencyService.getRecords();
+    //const worksheet: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
+    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(element);
+
+    const book: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(book, worksheet, 'Sheet1');
+
+    XLSX.writeFile(book, name);
+
+  }
 }
